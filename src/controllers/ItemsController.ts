52c50d6e4deb8568c789933ed172a3e153d43454
Rafael Mendes <rafaelthemendes@gmail.com>
2020-06-20 01:11:59 -0300

    changed item response

--- src/controllers/ItemsController.ts
+++ src/controllers/ItemsController.ts
@@ -4,9 +4,9 @@ import knex from "../database/connection";
 export default class ItemsController {
   async index(request: Request, response: Response) {
     const items = await knex("items").select("*");
-    const serializedItems = items.map(({ title, image }) => ({
-      title,
-      image: `http://localhost:3333/uploads/${image}`,
+    const serializedItems = items.map(({image, ...rest}) => ({
+      ...rest,
+      image_url: `http://localhost:3333/uploads/${image}`,
     }));
     return response.json(serializedItems);
   }
